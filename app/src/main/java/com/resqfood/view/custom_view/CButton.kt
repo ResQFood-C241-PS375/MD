343c297b8fb96450c8d0ab88ee4d209e19c616a9
Muhammad <muhammadahjahh@gmail.com>
2024-05-29 11:27:15 +0700

    post donation layout

--- app/src/main/java/com/resqfood/view/custom_view/CButton.kt
+++ app/src/main/java/com/resqfood/view/custom_view/CButton.kt
@@ -1,4 +1,20 @@
 package com.resqfood.view.custom_view
 
-class CButton {
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
+
+class CButton (context: Context, attributeSet: AttributeSet): AppCompatButton(context, attributeSet) {
+
+    init {
+        applyFont()
+    }
+
+    private fun applyFont() {
+        val typeface: Typeface =
+            Typeface.createFromAsset(context.assets,"Poppins.otf")
+        setTypeface(typeface)
+    }
+
 }

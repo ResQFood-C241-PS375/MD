3bfc55a46005bba51227c97a6841bcb476eaf3d9
Muhammad <muhammadahjahh@gmail.com>
2024-06-03 19:32:05 +0700

    register, welcome, sma splashscreen act

--- app/src/main/java/com/resqfood/view/register/RegisterViewModel.kt
+++ app/src/main/java/com/resqfood/view/register/RegisterViewModel.kt
@@ -1,7 +1,19 @@
 package com.resqfood.view.register
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.resqfood.repository.Repository
+import kotlinx.coroutines.launch
 
 class RegisterViewModel(private val repository: Repository) : ViewModel() {
+    private val _resultRegister = MutableLiveData<RegisterResponse>()
+    val resultRegister: LiveData<RegisterResponse> = _resultRegister
+
+    fun registerUser(name: String, email: String, password: String, phone: String) {
+        viewModelScope.launch {
+            _resultRegister.value = repository.registerUser(name, email, password, phone)
+        }
+    }
 }

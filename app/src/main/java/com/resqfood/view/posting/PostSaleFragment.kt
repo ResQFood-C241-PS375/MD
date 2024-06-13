ba842092aee62c384e50a925859bdbfb9036f1b5
Fathur Rasyid <fathurrasyid212@gmail.com>
2024-06-13 22:00:14 +0700

    Change toast to alertdialog

--- app/src/main/java/com/resqfood/view/posting/PostSaleFragment.kt
+++ app/src/main/java/com/resqfood/view/posting/PostSaleFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.resqfood.R
@@ -92,7 +93,7 @@ class PostSaleFragment : Fragment() {
                                     getString(R.string.label_1)
                                 }
                                 if (predictedLabel == "Bukan Roti") {
-                                    showToast("Predicted: Bukan Roti")
+                                    showAlert("Ini bukan foto roti!")
                                 }
                             }
                         }
@@ -145,8 +146,17 @@ class PostSaleFragment : Fragment() {
 //        }
     }
 
-    private fun showToast(message: String) {
-        Toast.makeText(requireActivity(), message, Toast.LENGTH_SHORT).show()
+    private fun showAlert(message: String) {
+        AlertDialog.Builder(requireActivity()).apply {
+            setTitle(getString(R.string.alert_title))
+            setIcon(R.drawable.ic_warning)
+            setMessage(message)
+            setPositiveButton("OKE") { dialog, _ ->
+                dialog.dismiss()
+            }
+            create()
+            show()
+        }
     }
 
     private fun showLoading(isLoading: Boolean) {
